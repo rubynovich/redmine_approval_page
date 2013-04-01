c327a0dd468be8d0fbf40be46e1d5fd5f442621c
rubynovich <roman@shipiev.me>
2013-04-01 10:02:02 +0400

    add: validate all_approved for Issue

--- lib/approval_page_issue_patch.rb
+++ lib/approval_page_issue_patch.rb
@@ -10,6 +10,8 @@ module ApprovalPagePlugin
       base.class_eval do
         has_many :approval_items
         has_many :approvers, :through => :approval_items, :foreign_key => :user_id, :class_name => "User", :uniq => true
+
+        validate :all_approved, :if => "!Issue.find(self.id).closed? && self.closed?"
       end
     end
 
@@ -17,7 +19,10 @@ module ApprovalPagePlugin
     end
 
     module InstanceMethods
-
+      def all_approved
+        return if approval_items.all?(&:approved?)
+        errors.add :base, I18n.t(:error_you_cant_close_issue_without_approval)
+      end
     end
   end
 end
