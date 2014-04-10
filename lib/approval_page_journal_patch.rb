6e7518bfb42e5046b20e9d8723fcda1f09d3d8fa
Maksim <max@koritskiy.ru>
2014-04-10 17:55:52 +0400

    fix

--- lib/approval_page_journal_patch.rb
+++ lib/approval_page_journal_patch.rb
@@ -2,8 +2,8 @@ module ApprovalPagePlugin
   module JournalPatch
     extend ActiveSupport::Concern
     included do
-      attr_accessor :approver_ids
-      attr_accessor :approval_action #:add, :destroy
+      attr_accessor :approvers
+      attr_accessor :approvals_action #:add, :destroy
     end
   end
 end
