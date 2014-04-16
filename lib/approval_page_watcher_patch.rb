9c5985009a5beea71f4be362bcb1a0a9419a8b24
Maksim <max@koritskiy.ru>
2014-04-16 18:23:07 +0400

    add is_watcher method

--- lib/approval_page_watcher_patch.rb
+++ lib/approval_page_watcher_patch.rb
@@ -2,7 +2,28 @@ module ApprovalPagePlugin
   module WatcherPatch
     extend ActiveSupport::Concern
     included do
-      attr_accessor :is_approver
+      attr_writer :is_approver
+      before_destroy :break_destroy_approver
     end
+
+    def is_approver
+      if self.watchable.kind_of?(Issue)
+        return @is_approver || self.watchable.approval_items.where(user_id: user_id).first.present?
+      else
+        return @is_approver
+      end
+    end
+
+    private
+    def break_destroy_approver
+      if is_approver
+        self.errors.add(:is_approver, :watcher_is_approver)
+        return false
+      end
+      true
+    end
+
+
+
   end
 end
