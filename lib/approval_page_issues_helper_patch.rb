ba69279c7718b7e2292c28b95526060676752614
Maksim <max@koritskiy.ru>
2014-04-18 14:51:52 +0400

    fix l to I18n class

--- lib/approval_page_issues_helper_patch.rb
+++ lib/approval_page_issues_helper_patch.rb
@@ -9,15 +9,15 @@ module ApprovalPagePlugin
       if detail.property == 'watchers'
         if detail.prop_key == 'approver' && ((detail.old_value.to_s.split(',') - detail.value.to_s.split(',')).count > 0)
           user = User.where(id: (detail.old_value.to_s.split(',') - detail.value.to_s.split(','))).first
-          return l(:message_remove_approver_gender, name: user.name)["g#{user.try(:gender).to_s}"].html_safe
+          return ::I18n.t(:message_remove_approver_gender, name: user.name)["g#{user.try(:gender).to_s}"].html_safe
         elsif ['approver','approvers'].include?(detail.prop_key) && ((detail.value.to_s.split(',') - detail.old_value.to_s.split(',')).count > 0)
           users = User.where(id: (detail.value.to_s.split(',') - detail.old_value.to_s.split(',')))
-          return (users.count > 1 ? l(:message_add_approvers, names: users.map(&:name).join(', ')) : l(:message_add_approver_gender, name: users.first.name)["g#{users.first.try(:gender).to_s}"] ).html_safe
+          return (users.count > 1 ? ::I18n.t(:message_add_approvers, names: users.map(&:name).join(', ')) : ::I18n.t(:message_add_approver_gender, name: users.first.name)["g#{users.first.try(:gender).to_s}"] ).html_safe
         elsif detail.prop_key == 'approved' && ((detail.value.to_s.split(',') - detail.old_value.to_s.split(',')).count > 0)
           #user = User.where(id: detail.old_value).first
-          return l(:message_approver_approved)[true].html_safe
+          return ::I18n.t(:message_approver_approved)[true].html_safe
         elsif detail.prop_key == 'approved' && ((detail.old_value.to_s.split(',') - detail.value.to_s.split(',')).count > 0)
-          return l(:message_approver_approved)[false].html_safe
+          return ::I18n.t(:message_approver_approved)[false].html_safe
         else
           show_detail_without_approval_page(detail, no_html, options)
         end
