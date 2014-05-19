0456c7d1a57938f067afeb8c22b70e5ef44696a1
Maksim <max@koritskiy.ru>
2014-05-19 17:11:46 +0400

    fix approval plugin

--- lib/approval_page_watcher_patch.rb
+++ lib/approval_page_watcher_patch.rb
@@ -2,13 +2,13 @@ module ApprovalPagePlugin
   module WatcherPatch
     extend ActiveSupport::Concern
     included do
-      attr_writer :is_approver
+      attr_writer :is_approver_not_approved
       before_destroy :break_destroy_approver
     end
 
-    def is_approver
+    def is_approver_not_approved
       if self.watchable.kind_of?(Issue)
-        return @is_approver || self.watchable.approval_items.where(user_id: user_id).first.present?
+        return @is_approver || self.watchable.approval_items.where(["user_id = ? and approved in (?)", user_id, [nil,false]]).first.present?
       else
         return @is_approver
       end
@@ -16,7 +16,7 @@ module ApprovalPagePlugin
 
     private
     def break_destroy_approver
-      if is_approver
+      if is_approver_not_approved
         self.errors.add(:is_approver, :watcher_is_approver)
         return false
       end
