eb517a4fff37729dedaaaf75f3109c01cc6c1815
Maksim <max@koritskiy.ru>
2014-05-20 10:09:27 +0400

    fux

--- lib/approval_page_watcher_patch.rb
+++ lib/approval_page_watcher_patch.rb
@@ -9,17 +9,17 @@ module ApprovalPagePlugin
 
     def is_approver_not_approved
       if self.watchable.kind_of?(Issue)
-        return @is_approver || self.watchable.approval_items.where(["user_id = ? and approved in (?)", user_id, [nil,false]]).first.present?
+        return @is_approver_not_approved || self.watchable.approval_items.where(["user_id = ? and approved in (?)", user_id, [nil,false]]).first.present?
       else
-        return @is_approver
+        return @is_approver_not_approved
       end
     end
 
     def is_approver
       if self.watchable.kind_of?(Issue)
-        return @is_approver || self.watchable.approval_items.where(user_id: user_id).first.present?
+        return @is_approver || @is_approver_not_approved || self.watchable.approval_items.where(user_id: user_id).first.present?
       else
-        return @is_approver
+        return @is_approver || @is_approver_not_approved
       end
     end
 
